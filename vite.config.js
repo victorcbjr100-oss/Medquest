49e96c722a94ae13828aa70c11ac073008bfc827
victorcbjr100-oss <victorcbjr100@gmail.com>
2026-03-28 17:02:36 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Na Vercel, geralmente não precisamos do "base", 
-  // a menos que você esteja usando um subdomínio específico.
-})
